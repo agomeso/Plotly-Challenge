e9a2807422298208b281379a0dd7e612e7d387f8
agomeso <alejandrogomezmba@gmail.com>
2021-04-30 23:41:56 -0500

    going to Advanced

--- static/js/app.js
+++ static/js/app.js
@@ -1,89 +1,121 @@
-// 1. Use the D3 library to read in samples.json.
-
+//Create a list of IDs to pass to the dropdown
 d3.json("../data/samples.json").then(function (data) {
-    //check console
-    // console.log(data);
-    // console.log(data.samples[0].sample_values);
-    // console.log(data.samples[0].otu_ids);
-    // console.log(data.samples[0].otu_labels);
-
-
-    //store then in a variable
-    // var NewArray = newData.filter(SortandSlice);
-    // console.log(NewArray)
-
-    var values = data.samples[0].sample_values.slice(0, 10).reverse();
-    console.log(values);
-    var ids = data.samples[0].otu_ids.slice(0, 10).reverse().map((id) => {
-        return `OTU ${id}`
-    });
-    console.log(ids);
-    var labels = data.samples[0].otu_labels.slice(0, 10).reverse();
-    console.log(labels);
-
-    // 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-    // Use sample_values as the values for the bar chart.
-    // Use otu_ids as the labels for the bar chart.
-    // Use otu_labels as the hovertext for the chart.
-    // Create your trace.
-    var tracebar = {
-        x: values,
-        y: ids,
-        hovertext: labels,
-        type: "bar",
-        orientation: "h"
-    };
-
-    // Create the data array for our plot
-    var databar = [tracebar];
-
-    // Define the plot layout
-    var layout = {
-        title: "Top 10 OTUs found in individual selected",
-        xaxis: { title: "# of things" },
-        yaxis: { title: "OTUs" }
-    };
-
-    // Plot the chart to a div tag with id "bar-plot"
-    Plotly.newPlot("bar", databar, layout);
-
-
-
-    // 3. Create a bubble chart that displays each sample.
-    // Use otu_ids for the x values.
-    // Use sample_values for the y values.
-    // Use sample_values for the marker size.
-    // Use otu_ids for the marker colors.
-    // Use otu_labels for the text values.
-
-    var tracebubble = {
-        x: data.samples[0].otu_ids,
-        y: data.samples[0].sample_values,
-        text: data.samples[0].otu_labels,
-        mode: 'markers',
-        marker: {
-            size: data.samples[0].sample_values,
-            color: data.samples[0].otu_ids,
-        }
-    };
-
-    var databubble = [tracebubble];
-
-    var layout = {
-        title: 'Bubble Chart',
-        showlegend: false,
-        height: 600,
-        width: 1200
-    };
-
-    Plotly.newPlot("bubble", databubble, layout);
+    console.log(data);
+    //Populate the dropdown
+    var dropdown = d3.selectAll("#selDataset")
+    var idlist = data.samples.map(d => d.id)
+    idlist.forEach(d => {
+        var o = dropdown.append("option");
+        o.text(d);
+    })
 
 });
 
-// 4. Display the sample metadata, i.e., an individual's demographic information.
+buildPage("940")
 
-// 5. Display each key - value pair from the metadata JSON object somewhere on the page.
+// On change to the dropdown, call getData()
+d3.selectAll("#selDataset").on("change", handleSubmit);
 
+// Submit Button handler
+function handleSubmit() {
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input value from the form
+    var idselected = d3.select("#selDataset").node().value;
+    // console.log(`User selected ${idselected}`);
+
+    // Build the page with the ID values
+    buildPage(idselected);
+}
+
+//Run function with selected value
+function buildPage(idselected) {
+
+    // 1. Use the D3 library to read in samples.json.
+    d3.json("../data/samples.json").then(function (data) {
+
+        var values = data.samples.filter(o => o.id == idselected)[0].sample_values.slice(0, 10).reverse();
+        // console.log(values);
+        var ids = data.samples.filter(o => o.id == idselected)[0].otu_ids.slice(0, 10).reverse().map((id) => {
+            return `OTU ${id}`
+        });
+        // console.log(ids);
+        var labels = data.samples.filter(o => o.id == idselected)[0].otu_labels.slice(0, 10).reverse();
+        // console.log(labels);
+
+        // 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+        // Use sample_values as the values for the bar chart.
+        // Use otu_ids as the labels for the bar chart.
+        // Use otu_labels as the hovertext for the chart.
+        // Create your trace.
+        var tracebar = {
+            x: values,
+            y: ids,
+            hovertext: labels,
+            type: "bar",
+            orientation: "h"
+        };
+
+        // Create the data array for our plot
+        var databar = [tracebar];
+
+        // Define the plot layout
+        var layout = {
+            title: "Top 10 OTUs found in individual selected",
+            xaxis: { title: "# of things" },
+            yaxis: { title: "OTUs" }
+        };
+
+        // Plot the chart to a div tag with id "bar-plot"
+        Plotly.newPlot("bar", databar, layout);
+
+
+
+        // 3. Create a bubble chart that displays each sample.
+        // Use otu_ids for the x values.
+        // Use sample_values for the y values.
+        // Use sample_values for the marker size.
+        // Use otu_ids for the marker colors.
+        // Use otu_labels for the text values.
+
+        var tracebubble = {
+            x: data.samples.filter(o => o.id == idselected)[0].otu_ids,
+            y: data.samples.filter(o => o.id == idselected)[0].sample_values,
+            text: data.samples.filter(o => o.id == idselected)[0].otu_labels,
+            mode: 'markers',
+            marker: {
+                size: data.samples.filter(o => o.id == idselected)[0].sample_values,
+                color: data.samples.filter(o => o.id == idselected)[0].otu_ids,
+            }
+        };
+
+        var databubble = [tracebubble];
+
+        var layout = {
+            title: 'Bubble Chart',
+            showlegend: false,
+            height: 600,
+            width: 1200
+        };
+
+        Plotly.newPlot("bubble", databubble, layout);
+
+        // 4. Display the sample metadata, i.e., an individual's demographic information.
+        // 5. Display each key - value pair from the metadata JSON object somewhere on the page.
+        var selection = d3.select("#sample-metadata").selectAll("h5").data(data.metadata.filter(o => o.id == idselected))
+
+        selection.enter()
+            .append("h5")
+            .merge(selection)
+            .html(function (d) {
+                // console.log(d);
+                return `<h5>id: ${d.id}</h5><h5> Ethnicity: ${d.ethnicity}</h5><h5> Gender: ${d.gender}</h5><h5> Age: ${d.age}</h5><h5> Location: ${d.location}</h5><h5> Wash Freq: ${d.wfreq}</h5>`
+            })
+
+
+    });
+}
 // 6. Update all of the plots any time that a new sample is selected.
 
 // Advanced Challenge Assignment(Optional)
